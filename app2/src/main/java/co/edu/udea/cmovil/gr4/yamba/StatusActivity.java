9140f94a7c764480611f23aa1c4c3d271ce8b097
agudelojuan <juanac07@gmail.com>
2015-05-07 06:43:42 -0500

    Initial commit

---
+++ app2/src/main/java/co/edu/udea/cmovil/gr4/yamba/StatusActivity.java
@@ -0,0 +1,30 @@
+package co.edu.udea.cmovil.gr4.yamba;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+
+public class StatusActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+// Check if this activity was created before
+        if (savedInstanceState == null) { //
+// Create a fragment
+            StatusFragment fragment = new StatusFragment(); //
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, fragment,
+                            fragment.getClass().getSimpleName())
+                    .commit(); //
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { //
+// Inflate the menu items to the action bar.
+        getMenuInflater().inflate(R.menu.menu_main, menu); //
+        return true; //
+    }
+}
